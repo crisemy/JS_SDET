c17bec0e494d85d208d234a13f521ee8f0b6e138
Cris N <crisemy@gmail.com>
2024-08-27 09:10:52 -0300

    Section 4

---
+++ Section4-Variables/variableLibrary.js
@@ -0,0 +1,16 @@
+// Adding 10 different variables and assigning them a proper 
+// String value based in the provided example.
+// After that print them out
+
+var line1 = "Bee in the Hive"
+var line2 = "The Secret life of the Ant"
+var line3 = "Laptops. Not just for Breakfast Anymore"
+var line4 = "If I were a Super Hero"
+var line5 = "Once upon a time in the Rabbit Hole"
+var line6 = "T"
+var line7 = "In Memory of the last Slice of Pizza"
+var line8 = "Who stole second base"
+var line9 = "2010. The coding Odyssey"
+var line10 = "The End"
+
+console.log(line1, "\n", line2, "\n", line3, "\n", line4,  "\n", line5,  "\n", line6, "\n", line7, "\n", line8, "\n", line9, line10)
---
+++ Section4-Variables/variables.js
@@ -0,0 +1,13 @@
+/* Variable naming rules
+1. Start any letter
+2. can't start with a number
+3. any lenght
+4. case sensitive
+5. use camel case (Eg. eyeColor)
+6. Reserved words (see the reservedWords.txt file) can NOT be used
+*/
+
+// defining variables. The data Type is a String in this case
+var eyeColor = 'blue'
+
+console.log(eyeColor)
