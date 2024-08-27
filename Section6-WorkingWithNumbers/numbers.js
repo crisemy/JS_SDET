ba566f40df930351d80f5a29552a6582e465cc60
Cris N <crisemy@gmail.com>
2024-08-27 09:12:19 -0300

    Section 6

---
+++ Section6-WorkingWithNumbers/numbers.js
@@ -0,0 +1,21 @@
+// Creating Var numbers
+
+var firstNumber = 1
+var secondNumber = 0.11
+var thirdNumber = 5123e5
+
+// Math operators (+ - * /)
+console.log(100 - 10)
+var addThis = 1 + 1
+console.log(addThis)
+
+// Special Math Operators (** Exponential, % Module, ++ increment)
+var increaseMe = 1
+var decreaseMe = 100
+increaseMe++
+console.log(increaseMe)
+
+// Standard practices for the Math order
+10+2*2 // 14
+(10+2)*2 // 24
+
