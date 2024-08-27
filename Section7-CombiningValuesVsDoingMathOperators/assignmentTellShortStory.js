eb0fc8940a9d97a124f69c3c88b102867e50d2a9
Cris N <crisemy@gmail.com>
2024-08-27 09:12:45 -0300

    Section 7

---
+++ Section7-CombiningValuesVsDoingMathOperators/assignmentTellShortStory.js
@@ -0,0 +1,10 @@
+//Here is the template for the story.
+// In a <place> not so far away, <personOrAnimal> saw that there was no <thing>.
+// Example story: "In a farm not so far away, chicken saw that there was no corn."
+
+var place = "farm"
+var personOrAnimal = "chicken"
+var thing = "corn."
+
+console.log("Example story: " + "In a " + place + " so far away, " + personOrAnimal 
++ " saw that there was no" + thing)
---
+++ Section7-CombiningValuesVsDoingMathOperators/combiningValues.js
@@ -0,0 +1,9 @@
+// Combining several values
+var stringValue = "1"
+var stringNumber = 1
+
+// Concatenation
+var firstPart = "Hello, my name is,"
+var secondPart = " John!"
+
+console.log(firstPart+secondPart)
